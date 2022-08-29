1a1dee73119fd13e3abfe6568a18ff0841e5b36a
ahmad <ahmadpafadnam1@gmail.com>
2022-08-29 23:56:12 +0000

    XP_NINJA

---
+++ week2/day1/exerciceXP_NINJA/exercice1/script.js
@@ -0,0 +1,30 @@
+5 >= 1
+//predication :true
+// actual :true
+0 === 1
+//predication :false
+// actual :false
+4 <= 1
+//predication :fales
+// actual :false
+1 != 1
+//predication :false
+// actual :false
+"A" > "B"
+//predication :false
+// actual :false
+"B" < "C"
+//predication :false
+// actual :true
+"a" > "A"
+//predication :true
+// actual :true
+"b" < "A"
+//predication :false
+// actual :false
+true === false
+//predication :false
+// actual :false
+true != true 
+//predication :false
+// actual :false
